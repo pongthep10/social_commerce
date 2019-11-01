8e38dca6f27b72b6b3b6af5dfa1b8692501578ed
pongthepnsq <pongthep@nsquared.asia>
2019-11-01 18:50:27 +0700

    add product ranking by url params

--- sell/models.py
+++ sell/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 class Item(models.Model):
     Name = models.CharField(max_length=200)
-    Url = models.URLField(max_length=200)
+    NSQID = models.CharField(max_length=64, unique=True)
+    ImageUrl = models.URLField(max_length=200)
     SellPrice = models.DecimalField( max_digits=5, decimal_places=2)
     Price = models.DecimalField( max_digits=5, decimal_places=2)
     Show = models.BooleanField(default=True)
+    def __str__(self):
+        return self.Name
 
 class Order(models.Model):
     status = models.CharField(max_length=10, blank=True, null=True, default='PENDING') #PENDING, SUBMITTED
     created_at = models.DateField(auto_now_add=True)
-    manychat_campaign = models.CharField(max_length=200, blank=True, null=True,)
+    manychat_campaign = models.CharField(max_length=200, blank=True, null=True)
     
-class Orderitems(models.Model):
-    OrderId = models.ForeignKey(Order, models.DO_NOTHING, blank=True, null=True)
-    ItemId =  models.ForeignKey(Item, models.DO_NOTHING, blank=True, null=True)
+class Orderitem(models.Model):
+    OrderId = models.ForeignKey(Order, on_delete = models.DO_NOTHING, blank=True, null=True)
+    ItemId =  models.ForeignKey(Item, on_delete = models.DO_NOTHING, blank=True, null=True)
     qty = models.IntegerField()
 
--- sell/views.py
+++ sell/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Item
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def items(request):
+    q = request.GET.getlist('p')
+    items = list(Item.objects.filter(NSQID__in=q))
+    items.sort(key=lambda t: q.index(t.NSQID))
+    template = loader.get_template('single_pdp/index.html')
+    # print('ddddddddddddddddddddd',)
+    context = {
+        'items': items,
+    }
+    return HttpResponse(template.render(context, request))
