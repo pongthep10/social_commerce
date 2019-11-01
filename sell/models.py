bb792e532b207a81eb7ac1244632b68d6146557a
pongthepnsq <pongthep@nsquared.asia>
2019-11-01 16:14:23 +0700

    first

--- sell/models.py
+++ sell/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-class Items(models.Model):
+class Item(models.Model):
     Name = models.CharField(max_length=200)
+    Url = models.URLField(max_length=200)
     SellPrice = models.DecimalField( max_digits=5, decimal_places=2)
     Price = models.DecimalField( max_digits=5, decimal_places=2)
     Show = models.BooleanField(default=True)
 
-class Orders(models.Model):
-    is_submitted = models.BooleanField(default=False)
-
-class ItemsOrders(models.Model):
-    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+class Order(models.Model):
+    status = models.CharField(max_length=10, blank=True, null=True, default='PENDING') #PENDING, SUBMITTED
+    created_at = models.DateField(auto_now_add=True)
+    manychat_campaign = models.CharField(max_length=200, blank=True, null=True,)
+    
+class Orderitems(models.Model):
+    OrderId = models.ForeignKey(Order, models.DO_NOTHING, blank=True, null=True)
+    ItemId =  models.ForeignKey(Item, models.DO_NOTHING, blank=True, null=True)
+    qty = models.IntegerField()
 
